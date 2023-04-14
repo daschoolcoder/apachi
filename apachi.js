c93ae7630dbf5e3be535b21b1b88b1e8ae3b6cd1
coder <102377044+daschoolcoder@users.noreply.github.com>
2023-04-14 16:46:06 +0000

    Update apachi.js

--- apachi.js
+++ apachi.js
@@ -11,7 +11,7 @@ function Apachi_Open() {
 
  <div style='background-color:rgb(255,0,0); width:100%;height:100%; z-index:0 '>
   <div>
- <center>  <h1> Apachi V1.5.4 Alpha </h1> </center> 
+ <center>  <h1> Apachi V1.5.5 Alpha </h1> </center> 
          </div> 
    
    
@@ -63,7 +63,7 @@ o.style= "background-color:rgb(0,255,0)";
                 '> 1v1 lol </button>    
         
         
-             <button style='background-color:rgb(111,111,111);' onclick='var iframe = document.createElement("iframe"); iframe.src="https://script.google.com/macros/s/AKfycbya1HhgYEWr4fXKrikh0H9A7VOLVjnCsUzFkYdYrECQBnHA4Q/exec"; iframe.style=" width:54%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
+             <button style='background-color:rgb(111,111,111);' onclick='var iframe = document.createElement("iframe"); iframe.src="https://3ghost-deploy-seven.vercel.app/projects/slope/index.html"; iframe.style=" width:100%; height:100%";document.body.appendChild(iframe); var d = document.createElement("button");
              d.onclick= function () {document.body.removeChild(iframe);document.body.removeChild(d) ;};
              d.innerHTML="X";
              d.style="background-color:red";
