2b004ed8d418038d304b5e137291d6890950adec
coder <102377044+daschoolcoder@users.noreply.github.com>
2023-04-26 17:04:12 +0000

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,3 +1,4 @@
-javascript:req = new XMLHttpRequest(); 
-req.open('GET', 'https://raw.githubusercontent.com/daschoolcoder/apachi/main/apachi.js'); 
+req = new XMLHttpRequest(); 
+req.open('GET', 'https://raw.githubusercontent.com/daschoolcoder/apachi/apachi-pro/Apachi-PRO.js'); 
 req.onload = function() { eval(this.responseText ); }; req.send();
+
